b5d1170bdfc19b36d7e70afa3943e6cab02badc9
Noah Liebenberg <noah.liebenberg@charthouse.co.uk>
2023-06-17 23:43:15 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Organization Site/src/partials/About Section/About.jsx
    #	Organization Site/src/partials/Project Section/ProjectsSection.jsx

--- Organization Site/src/pages/ProjectPages/WinePage.jsx
+++ Organization Site/src/pages/ProjectPages/WinePage.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './ProjectPages.css';
+import NavBar from "../../partials/Nav Bar/NavBar";
+import HeaderNavBar from "../../partials/Header-Nav-Bar";
+import HeroHome from "../../partials/Hero Section/HeroHome";
+import FeaturedSection from "../../partials/Featured Section/FeaturedSection";
+import About from "../../partials/About Section/About";
+import ProjectsSection from "../../partials/Project Section/ProjectsSection";
+import Contact from "../../partials/Contact Section/Contact";
+import Footer from "../../partials/Footer Section/Footer";
 
 const WinePage = () => {
     return (
-        <section className="WinePage">
-        <section className="HeroHome-section" id="HeroHome">
-            <h1 className="Main-Title"><h1 className="Main-Title-smaller">Lieben</h1>LOGISTICS</h1>
-            <p className="Main-Slogan"> Page in development </p>
-        </section>
-        </section>
+            <section className="WinePage flex flex-col min-h-screen overflow-hidden">
+                <NavBar/>
+                {/*  Page content */}
+                <main className="grow">
+
+
+
+
+                </main>
+
+                {/*  Site footer */}
+                <Footer />
+            </section>
+
     );
 };
 
