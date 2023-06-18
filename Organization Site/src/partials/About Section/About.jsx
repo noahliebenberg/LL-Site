8a0da48508ca0281520bea15c2e00f2082082380
Noah Liebenberg <noah.liebenberg@charthouse.co.uk>
2023-06-18 18:56:10 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Organization Site/src/partials/About Section/About.jsx
    #	Organization Site/src/partials/Project Section/ProjectsSection.jsx

--- Organization Site/src/partials/About Section/About.jsx
+++ Organization Site/src/partials/About Section/About.jsx
@@ -3,7 +3,7 @@ import ProfileCard from './ProfileCard'; // import the new component at the top
 import './About.css';
 
 import paProfile from '../../images/PaProfile.jpg';
-import yochanan from '../../images/Yochanan.jpg';
+import yochanan from '../../images/Yochanan.jpeg';
 import noah from '../../images/Noah.jpg';
 import benjamin from '../../images/Benjamin.png';
 
@@ -27,11 +27,10 @@ class About extends Component {
                 <div className="About-left-text">
                     <div className="About-title"><h1>About<span className="grey"> us </span></h1></div>
                     <div className="About-text">
-                        As a team, we are a group of three professionals with diverse backgrounds and expertise. Our skills cover various areas including computer science, data science, and law. We are passionate about continuous learning and tackling complex challenges, and are committed to delivering solutions that are data-driven. Our team consists of a Computer Science Honours graduate and Master's candidate in Data Science specializing in Machine Learning, a Computer Science Honours graduate with expertise in C#, Python, and JavaScript, and a first-class honours LLB graduate studying law at Swansea University.
+                        Our team is an assembly of four professionals with diverse backgrounds and expertise. Our skills encompass various fields including computer science, data science, law, and entrepreneurship. We are passionate about continuous learning, tackling complex challenges, and are committed to delivering data-driven solutions. The team is composed of a Computer Science Honours graduate and Master's candidate in Data Science specializing in Machine Learning, a Computer Science Honours graduate with expertise in C#, Python, and JavaScript, a first-class honours LLB graduate studying law at Swansea University, and an accomplished Entrepreneur who founded several successful ventures including Worldwide Brochure Bank, Nexus Connections, e-locate, Tsitsikamma Mountain Reserve, and Stonewall Farm.
                     </div>
                     <div className="About-text">
-                        Together, we bring a wealth of knowledge and experience to any project we undertake. We work collaboratively to leverage our diverse skill sets to achieve our goals, with a shared commitment to excellence. We are enthusiastic, dedicated, and bring a high level of professionalism to our work, making us a valuable asset to any organization. We are excited about the opportunities that come our way, and we are always looking for new challenges to take on.
-                    </div>
+                        Together, we bring an extensive wealth of knowledge and experience to any project we undertake. We collaborate effectively to harness our diverse skill sets to reach our objectives, unified in our commitment to excellence. We are enthusiastic, dedicated, and maintain a high degree of professionalism in our work, positioning us as a valuable asset to any organization. The opportunities that cross our paths ignite our excitement, and we constantly seek new challenges to undertake.                    </div>
                 </div>
                 <div className="About-right-icons">
                     <div className="About-blocks-container">
@@ -39,7 +38,7 @@ class About extends Component {
                             imgSrc={paProfile}
                             altText="Pa Profile"
                             name="Yochanan Liebenberg"
-                            description="Entrepreneur and business mogul. Developed E-locate, Nexus, Tsitsikamma Mountain Reserve, Stone Wall Farm..."
+                            description="Entrepreneur who founded several successful ventures including Worldwide Brochure Bank, Nexus Connections, e-locate, Tsitsikamma Mountain Reserve, and Stonewall Farm."
                             isFlipped={this.state.flippedCard === "Yochanan Liebenberg"}
                             onClick={() => this.handleClick("Yochanan Liebenberg")}
                             phone="+447724604324"
@@ -61,7 +60,7 @@ class About extends Component {
                             imgSrc={noah}
                             altText="Noah"
                             name="Noah Liebenberg"
-                            description="I’m a recent Exeter University Computer Science Honours graduate currently completing a Masters in a data science at Swansea University."
+                            description="I'm a Computer Science Honors graduate with four years of higher education experience, currently working at ChartHouse Data Management Ltd."
                             isFlipped={this.state.flippedCard === "Noah Liebenberg"}
                             onClick={() => this.handleClick("Noah Liebenberg")}
                             phone="+4407526226401"
--- Organization Site/src/partials/Footer Section/Footer.jsx
+++ Organization Site/src/partials/Footer Section/Footer.jsx
@@ -4,30 +4,45 @@ import { Link } from 'react-router-dom';
 import './FooterSection.css'
 
 function Footer() {
-  return (
-    <footer>
+    const experienceList = [
+        "Entrepreneurship",
+        "Deployment",
+        "Contract Law",
+        "Cloud Hosting",
+        "Startups",
+        "Mobile Apps",
+        "Legal Compliance",
+        "Desktop Apps",
+        "Business Development",
+        "Security",
+        "Strategic Planning",
+        "Product Management",
+        "Leadership",
+        "Data Analysis",
+        "UX/UI Design",
+        "Digital Marketing",
+        "Machine Learning",
+    ];
 
-      <div className="bottom-strip-container">
-        <div className="title">EXPERIANCED IN:</div>
-        <ul>
-            <div className="icon-title">Entrepreneurship</div>
-            <div className="icon-title">React Native</div>
-            <div className="icon-title">Contract Law</div>
-            <div className="icon-title">Python</div>
-            <div className="icon-title">Startups</div>
-            <div className="icon-title">C#</div>
-            <div className="icon-title">Legal Compliance</div>
-            <div className="icon-title">CSS</div>
-            <div className="icon-title">Business Development</div>
-            <div className="icon-title">React</div>
-            <div className="icon-title">Java</div>
-            <div className="icon-title">Strategic Planning</div>
-            <div className="icon-title">Vue</div>
-            <div className="icon-title">Product Management</div>
-            <div className="icon-title">Leadership</div>
-            <div className="icon-title">Data Analysis</div>
-        </ul>
-      </div>
+  return (
+      <footer>
+
+          <div className="bottom-strip-container">
+              <div className="title">EXPERIANCED IN:</div>
+              <div className="marquee">
+                  {experienceList.map((item, i) => (
+                      <div key={i} className="marquee-item">
+                          {item}
+                      </div>
+                  ))}
+                  {/* Duplicating elements for continuous scroll */}
+                  {experienceList.map((item, i) => (
+                      <div key={i + experienceList.length} className="marquee-item">
+                          {item}
+                      </div>
+                  ))}
+              </div>
+          </div>
 
       <div className="footer-container">
           <div className="Footer-Logo">
